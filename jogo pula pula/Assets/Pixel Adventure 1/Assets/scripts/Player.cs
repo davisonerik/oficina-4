b8254ec88c713bfb67f62163404b3f038632ba19
davisonerik <145374995+davisonerik@users.noreply.github.com>
2024-08-16 10:55:39 -0300

    fase 4

--- jogo pula pula/Assets/Pixel Adventure 1/Assets/scripts/Player.cs
+++ jogo pula pula/Assets/Pixel Adventure 1/Assets/scripts/Player.cs
@@ -12,6 +12,7 @@ public class Player : MonoBehaviour
     public bool doubleJump;
 
     private Rigidbody2D rig;
+    private Animator anim;
 
 
     
@@ -20,6 +21,7 @@ public class Player : MonoBehaviour
     void Start()
     {
         rig = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -32,6 +34,25 @@ public class Player : MonoBehaviour
     {
         Vector3 movemment = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
         transform.position += movemment * Time.deltaTime * speed;
+        
+        if(Input.GetAxis("Horizontal") > 0f)
+        {
+            anim.SetBool("walk", true); 
+            transform.eulerAngles = new Vector3(0f,0f,0f);
+        }
+
+        if(Input.GetAxis("Horizontal") < 0f)
+        {
+            anim.SetBool( "walk", true);
+             transform.eulerAngles = new Vector3(0f,180f,0f); 
+        }
+
+        if(Input.GetAxis("Horizontal") == 0)
+        {
+            anim.SetBool("walk", false); 
+        }
+        
+        
     }
 
     void Jump()
@@ -42,6 +63,8 @@ public class Player : MonoBehaviour
             {
                rig.AddForce(new Vector2(0f, JumpForce), ForceMode2D.Impulse);
                doubleJump = true;
+               anim.SetBool( "Jump", true);
+
             }
             else
             {
@@ -60,6 +83,8 @@ public class Player : MonoBehaviour
        if(collision.gameObject.layer == 8)
        {
          isJumping = false;
+         anim.SetBool( "Jump", false);
+
        }
     }
 
